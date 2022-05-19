34e60247c7da75f76b666efd4c567801d1b127e3
Raúl Villares <raulvillares@gmail.com>
2022-05-19 16:07:03 +0200

    Initial commit

---
+++ Wrapper.Tests/WordWrapperShould.cs
@@ -0,0 +1,10 @@
+using Xunit;
+using exeal.word_wrapper_kata;
+
+namespace exeal.word_wrapper_kata.tests.wrapper
+{
+    public class WordWrapperShould
+    {
+
+    }
+}
---
+++ Wrapper/Wrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exeal.word_wrapper_kata
+{
+    public class Wrapper
+    {
+        public String wrap(String input, int columns)
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
